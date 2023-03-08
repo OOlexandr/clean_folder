f006a71b01d0f1ea66adc5fce837a97fbe3025d8
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-03-08 11:50:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='clean_folder',
       version='1.2',
-      url='http://github.com/dummy_user/useful',
+      url='https://github.com/OOlexandr/clean_folder',
       author_email='oleksarkhumua@gmail.com',
       packages=['clean_folder'],
       entry_points={'console_scripts': ['clean_folder = clean_folder.clean']})
